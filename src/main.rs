00d127a2b16041d4f0d16e6aabef7585ede96bb5
Gustav Sundin <sundin666@gmail.com>
2020-11-27 08:13:18 +0100

    Compare guess using match expression

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -10,11 +11,29 @@ fn main() {
 
     println!("Please input your guess.");
 
+    // Variables are immutable by default
     let mut guess = String::new();
 
+    // expect = error handling
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    // Shadow the original guess variable by casting it to an unsigned int
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    // A match expression is made up of arms. 
+    // An arm consists of a pattern and the code that should be run if the value matches that arm’s pattern.
+    //
+    // This comparison means that Rust can infer that the type of secret_number should be u32 (default is i32)
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
